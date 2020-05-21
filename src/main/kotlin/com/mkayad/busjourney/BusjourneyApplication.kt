9a65ba1fd2ced5e77637f214d965b3ae7285cb44
Moussa Kayad <mkayad@gmail.com>
2020-05-21 10:06:10 +0100

    added project files

---
+++ src/main/kotlin/com/mkayad/busjourney/BusjourneyApplication.kt
@@ -0,0 +1,11 @@
+package com.mkayad.busjourney
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BusjourneyApplication
+
+fun main(args: Array<String>) {
+	runApplication<BusjourneyApplication>(*args)
+}
---
+++ src/test/kotlin/com/mkayad/busjourney/BusjourneyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mkayad.busjourney
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BusjourneyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
