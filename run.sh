617a0e90b0a25e08d54c22f3acdbb858e9a94d13
Moussa Kayad <mkayad@gmail.com>
2020-06-16 17:13:17 +0100

    added run file

---
+++ run.sh
@@ -0,0 +1,2 @@
+java -jar -Dstop.g.url=https://api.tfl.gov.uk/StopPoint/490000091G/arrivals?mode=bus  -Dstop.h.url=https://api.tfl.gov.uk/StopPoint/490000091H/arrivals?mode=bus target/busjourney.jar
+
