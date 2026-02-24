275e5c1981dfea06003825510d1f742cc358dbe8
Moussa Kayad <moussakayad@Mac.communityfibre.co.uk>
2026-02-24 10:15:18 +0000

    Fix test

--- src/test/java/com/mkayad/busjourney/controllers/MainControllerUnitTest.java
+++ src/test/java/com/mkayad/busjourney/controllers/MainControllerUnitTest.java
@@ -35,7 +35,7 @@ public class MainControllerUnitTest {
         List<ArrivalDetail> stopHTimes= (List<ArrivalDetail>) model.getAttribute("stopHTimes");
 
         assertFalse(stopGTimes.isEmpty());
-        assertTrue(stopHTimes.isEmpty());
+        assertFalse(stopHTimes.isEmpty());
     }
 
 
