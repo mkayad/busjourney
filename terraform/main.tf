e0f71898b2e487586188cbc07a9684df36a28bd9
Moussa Kayad <moussakayad@Moussas-MacBook-Pro.local>
2026-02-27 15:31:02 +0000

    Disable test for pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,7 +106,7 @@ resource "azurerm_mysql_flexible_database" "main" {
 
 ## Azure Key Vault
 resource "azurerm_key_vault" "main" {
-  name                = "${var.app_service_name}_kv"
+  name                = "${var.app_service_name}-kv"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   tenant_id           = data.azurerm_client_config.current.tenant_id
